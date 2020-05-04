de70864df9b19376dce3e494bf71c8a13cced268
Kirillosman <61022625+Kirillosman@users.noreply.github.com>
2020-05-04 18:02:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,110 @@
+let studentKolya = {
+    firstName: "Kolya", 
+    lastName: "Ivanov", 
+    age: 19, 
+    grades: [5,3,2,5,4], 
+    course: 2, 
+    gender: "male",
+};
+let studentPetya = {
+    firstName: "Petya", 
+    lastName: "Ivanov", 
+    age: 21, 
+    grades: [4,2,2,5,5], 
+    course: 4, 
+    gender: "male",
+};
+let studentAnna = {
+    firstName: "Anna", 
+    lastName: "Ivanova", 
+    age: 20, 
+    grades: [4,2,2,5,5], 
+    course: 3, 
+    gender: "female",
+};
+let studentAnton = {
+    firstName: "Anton", 
+    lastName: "Petrov", 
+    age: 20, 
+    grades: [2,2,2,4,5], 
+    course: 3, 
+    gender: "male",
+};
+let studentVasya = {
+    firstName: "Vasya", 
+    lastName: "Semenov", 
+    age: 22, 
+    grades: [2,1,2,3,4], 
+    course: 5, 
+    gender: "male",
+};
+let studentVova = {
+    firstName: "Vova", 
+    lastName: "Chibrik", 
+    age: 18, 
+    grades: [1,2,1,4,2], 
+    course: 1, 
+    gender: "male",
+};
+let studentOlya = {
+    firstName: "Olya", 
+    lastName: "Kartopla", 
+    age: 19, 
+    grades: [1,2,3,4,2], 
+    course: 2, 
+    gender: "female",
+};
+let studentKostya = {
+    firstName: "Kostya", 
+    lastName: "Rembo", 
+    age: 21, 
+    grades: [3,4,4,4,3], 
+    course: 4, 
+    gender: "male",
+};
+let studentArtem = {
+    firstName: "Artem", 
+    lastName: "Panda", 
+    age: 18, 
+    grades: [3,4,5,4,5], 
+    course: 1, 
+    gender: "male",
+};
+let studentMasha = {
+    firstName: "Masha", 
+    lastName: "Nevasha", 
+    age: 19, 
+    grades: [2,2,3,4,3], 
+    course: 2, 
+    gender: "female",
+};
+
+let arrayStudents = [studentKolya, studentPetya, studentAnna, studentAnton, studentVasya, studentVova, studentOlya, studentKostya, studentArtem, studentMasha];
+
+for (let i = 0; i < arrayStudents.length; i = i + 1) {
+    let sum = 0;
+    for(let j = 0; j < arrayStudents[i].grades.length; j = j + 1) {
+    sum = sum + arrayStudents[i].grades[j];
+    };
+    let avg = sum / arrayStudents[i].grades.length;
+    console.log(avg);
+    for (let k = 0; k < arrayStudents.length;  k++) {
+        if (avg <= 3 && arrayStudents.gender == "male" && arrayStudents.age >= 18){
+        arrayStudents[k].isReadyForArmy = true;
+        delete arrayStudents[k].grades;
+        delete arrayStudents[k].course;
+        console.log(arrayStudents[k]);
+        };
+    };
+    // let Army = [];
+    // for (let x = 0; x < arrayStudents.length; x++) {
+    //     if (arrayStudents[x].isReadyForArmy = true) {
+    //         Army.push(arrayStudents[x])
+    //         console.log(Army);
+    //     }
+    // }
+ };
+ 
+
+
+
